ee82868ed4a86bca2528b1f17e2bac1160f078ad
Zaigham Faraz Siddiqui <zaighamfaraz.siddiqui@trumpf.com>
2022-09-28 10:13:05 +0200

    [feature] add timer

--- QueueTrigger/__init__.py
+++ QueueTrigger/__init__.py
@@ -1,8 +1,9 @@
 import logging
-
+import time
 import azure.functions as func
 
 
 def main(msg: func.QueueMessage) -> None:
     logging.info('Python queue trigger function processed a queue item: %s',
                  msg.get_body().decode('utf-8'))
+    time.sleep(1)
