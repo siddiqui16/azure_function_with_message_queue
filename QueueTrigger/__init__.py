fa8dc37569c17cae7e2a47b2baf74c5b580967ff
Zaigham Faraz Siddiqui <zaighamfaraz.siddiqui@trumpf.com>
2022-09-28 12:55:04 +0200

    [feature] complex

--- QueueTrigger/__init__.py
+++ QueueTrigger/__init__.py
@@ -1,9 +1,15 @@
 import logging
 import time
+import random
 import azure.functions as func
 
 
 def main(msg: func.QueueMessage) -> None:
-    logging.info('Python queue trigger function processed a queue item: %s',
+    logging.info('Python queue trigger function processed a queue item: %s', 
                  msg.get_body().decode('utf-8'))
-    time.sleep(1)
+    prod_a = []
+    for i in range(random.randint(1,100)):
+        prod_a += [i*i]
+    res = sum(prod_a) 
+    logging.info(f'Result = {res}')
+    time.sleep(5)
